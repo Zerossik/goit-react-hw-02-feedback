e6f8168071241d32c1da28c5d25c090912502a0c
Zerossik <zeross19940303@gmail.com>
2023-05-08 23:02:29 +0300

    main

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import FriendsElement from './FriendList/FriendList';
+import friends from '../data/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendsElement friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
